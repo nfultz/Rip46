9195dc89ac91d3d854bf01fd78404585943f519e
njf2 <njf2@sd699.zestcash.org>
2014-12-11 21:00:33 +0000

    Adding subsetting for ip vectors.

--- R/rip4.R
+++ R/rip4.R
@@ -7,3 +7,12 @@ format.ip4 <- function(x, ...) ip4ToHost(x)
 
 #' @export
 print.ip4 <- function(x) print(ip4ToHost(x))
+
+#' @export
+`[.ip4` <- function(x, ...) `oldClass<-`( unclass(x)[...], oldClass(x))
+
+#' @export
+`[<-.ip4` <- function(x,i, value) {
+  f <- switch(storage.mode(value), integer=identity, double=mySqlToIp4, character=hostToIp4, stop)
+  `oldClass<-`( `[<-`(unclass(x), i, f(value)), oldClass(x))
+}
